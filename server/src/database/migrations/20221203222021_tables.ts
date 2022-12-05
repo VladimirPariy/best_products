9a0889ad0f6c1135ea1dff1c4c95db67e4da04b4
VladimirPariy <vladimir.pariy@gmail.com>
2022-12-05 14:49:02 +0200

    added other tables in migration

--- server/src/database/migrations/20221203222021_tables.ts
+++ server/src/database/migrations/20221203222021_tables.ts
@@ -16,7 +16,7 @@ export async function up(knex: Knex): Promise<void> {
     table.string("phone_number", 15).unique().notNullable();
     table.string("user_photo");
     table.timestamps(true);
-    table.integer("role").unsigned();
+    table.integer("role").unsigned().notNullable();
     table.foreign("role").references("role_id").inTable("roles");
   })
   .createTable("product", (table) => {
@@ -43,18 +43,90 @@ export async function up(knex: Knex): Promise<void> {
     table.foreign("product").references("product_id").inTable("product");
     table.foreign("characteristic").references("characteristic_id").inTable("characteristics");
   })
-
+  .createTable("price_history", (table) => {
+    table.increments("price_history_id");
+    table.integer("product").notNullable().unsigned();
+    table.decimal("price_at_timestamp", 10, 2).notNullable();
+    table.timestamps(true);
+    table.foreign("product").references("product_id").inTable("product");
+  })
+  .createTable("feedback_types", (table) => {
+    table.increments("feedback_type_id");
+    table.integer("type").notNullable();
+  })
+  .createTable("feedback", (table) => {
+    table.integer("user").notNullable().unsigned();
+    table.integer("product").notNullable().unsigned();
+    table.integer("feedback_type").notNullable().unsigned();
+    table.timestamps(true);
+    table.foreign("feedback_type").references("feedback_type_id").inTable("feedback_types");
+    table.foreign("user").references("user_id").inTable("users");
+    table.foreign("product").references("product_id").inTable("product");
+    table.primary(["user", "product"])
+  })
+  .createTable("views", (table) => {
+    table.increments("view_id")
+    table.integer("user").notNullable().unsigned();
+    table.integer("product").notNullable().unsigned();
+    table.timestamps(true);
+    table.foreign("user").references("user_id").inTable("users");
+    table.foreign("product").references("product_id").inTable("product");
+  })
+  .createTable("favourite_prod", (table) => {
+    table.integer("user").unsigned().notNullable();
+    table.integer("product").unsigned().notNullable();
+    table.timestamps(true);
+    table.foreign("user").references("user_id").inTable("users");
+    table.foreign("product").references("product_id").inTable("product");
+    table.primary(["user", "product"])
+  })
+  .createTable("comments", (table) => {
+    table.increments("comments_id");
+    table.integer("user").notNullable().unsigned();
+    table.integer("product").notNullable().unsigned();
+    table.string("comments_msg", 255).notNullable();
+    table.timestamps(true);
+    table.foreign("user").references("user_id").inTable("users");
+    table.foreign("product").references("product_id").inTable("product");
+  })
+  .createTable("categories", (table) => {
+    table.increments("category_id");
+    table.string("category_title", 45).notNullable();
+  })
+  .createTable("subcategories", (table) => {
+    table.increments("subcategory_id");
+    table.string("subcategory_title", 45).notNullable();
+    table.integer("category").unsigned().notNullable();
+    table.foreign("category").references("category_id").inTable("categories");
+  })
+  .createTable("prod_subcategories", (table) => {
+    table.integer("product").unsigned().notNullable();
+    table.integer("subcategory").unsigned().notNullable();
+    table.foreign("subcategory").references("subcategory_id").inTable("subcategories");
+    table.foreign("product").references("product_id").inTable("product");
+    table.primary(["product", "subcategory"]);
+  })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
   return knex.schema
-  .dropTable("users")
-  .dropTable("roles")
+  .dropTable("comments")
+  .dropTable("favourite_prod")
+  .dropTable("feedback")
+  .dropTable("feedback_types")
+  .dropTable("views")
   .dropTable("product_images")
   .dropTable("prod_characteristic")
+  .dropTable("price_history")
+  .dropTable("prod_subcategories")
+  .dropTable("subcategories")
+
+  .dropTable("categories")
   .dropTable("characteristics")
   .dropTable("product")
+  .dropTable("users")
+  .dropTable("roles")
 }
 
 
